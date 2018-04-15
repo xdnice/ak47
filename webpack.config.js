982b5ad5bb986511e77f927c29381b831b7bbdbb
xuya227939 <1245244711@qq.com>
2018-04-15 11:52:57 +0800

    Add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pkg = require('./package.json');
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  externals: {React: 'react'},
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'babel-preset-env', 'stage-0'],
+            plugins: ["transform-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
