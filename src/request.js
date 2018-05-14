10ffbe12c87630a5e0bfc57048a49a70280a4423
Thomas <liujiang@landleaf-tech.com>
2018-05-14 16:56:32 +0800

    更改接口，fuck3000

--- src/request.js
+++ src/request.js
@@ -5,7 +5,9 @@
 import { message } from 'antd';
 //生产环境还是开发环境
 // const getApi = process.env.NODE_ENV === 'production' ? 'http://downfuture.com/' : 'http://localhost:8080/';
-const getApi = 'http://47.98.231.165:8080/';
+const getApi = 'http://47.98.231.165:8000/';
+
+// const getApi = 'http://localhost:3000/';
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
