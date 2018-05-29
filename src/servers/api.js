7f8faf4f1c7f4cbddd9d9c71920e50aa4d7e865d
xuya227939 <1245244711@qq.com>
2018-05-29 16:45:44 +0800

    修改命名规范

--- src/servers/api.js
+++ src/servers/api.js
@@ -2,7 +2,7 @@ import { requestPost } from '../request';
 
 export function fetchData(params) {
   return requestPost({
-    method: 'user',
+    method: 'home',
     options: {
       body: {
         count: params.count
